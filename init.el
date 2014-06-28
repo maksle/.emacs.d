841919322cf4fb1bbf603866507a8bba05bc1c0a
Maksim Grinman <maxchgr@gmail.com>
2014-06-28 15:00:15 -0400

    add users dir

--- init.el
+++ init.el
@@ -73,3 +73,13 @@
 ;; Setup key bindings
 (require 'key-bindings)
 (put 'scroll-left 'disabled nil)
+
+;; Settings for currently logged in user
+(setq user-settings-dir
+      (concat user-emacs-directory "users/" user-login-name))
+
+;; Conclude init by setting up specifics for the current user
+(when (file-exists-p user-settings-dir)
+  (mapc 'load (directory-files user-settings-dir nil "^[^#].*el$")))
+
+
